c1a027bcad94e026ad5edb577859b41508ffcea5
Adimanav2000 <110248132+Adimanav2000@users.noreply.github.com>
2022-11-19 21:56:20 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    int i = 0;
+
+    //for loop
+
+    for (int i = 0; i < 11; i++){
+        cout<<i<<"\n";
+
+    }
+
+    // while loop
+
+    while (i < 11) {
+        cout<<i<<"\n";
+        i++;
+    }
+
+    return 0;
+}
